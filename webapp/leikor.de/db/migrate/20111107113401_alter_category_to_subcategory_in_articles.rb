296975d80b16a69cb26387fe5132de8e825c1d98
Arne Ludwig <ludwig.arne@googlemail.com>
2011-11-16 22:31:27 +0100

    removed old webapp/leikor.de

--- webapp/leikor.de/db/migrate/20111107113401_alter_category_to_subcategory_in_articles.rb
+++
@@ -1,9 +0,0 @@
-class AlterCategoryToSubcategoryInArticles < ActiveRecord::Migration
-  def self.up
-    rename_column :articles, :category_id, :subcategory_id
-  end
-
-  def self.down
-    rename_column :articles, :subcategory_id, :category_id
-  end
-end
