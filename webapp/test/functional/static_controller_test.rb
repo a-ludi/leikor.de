18ba9c9542180ea359a5e91f7dc2f05b3bd26c75
Arne Ludwig <ludwig.arne@googlemail.com>
2011-11-22 11:44:00 +0100

    FunctionalTest: two cases added at StaticController

--- webapp/test/functional/static_controller_test.rb
+++ webapp/test/functional/static_controller_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "accessing unknown page raises error" do
+    assert_raises ActionController::RoutingError do
+      get 'show', {:path => "unkown_page"}
+    end
+    
+    assert_raises ActionController::RoutingError do
+      get 'show', {:path => nil}
+    end
+  end
+
+  test "accessing unknown page raises error with the path" do
+    begin
+      get 'show', {:path => 'unkown_page'}
+    rescue ActionController::RoutingError
+      assert_not_nil $!.inspect['unkown_page']
+    end    
   end
 end
