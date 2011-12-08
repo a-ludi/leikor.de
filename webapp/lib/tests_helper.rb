77cd85c07de3341d75a30d864b8602e3725fc4a1
Arne Ludwig <ludwig.arne@googlemail.com>
2011-12-08 12:37:26 +0100

    Miraculous, but all test are running

---
+++ webapp/lib/tests_helper.rb
@@ -0,0 +1,12 @@
+# -*- encoding : utf-8 -*-
+
+module TestsHelper
+  def with_user(user=:john, session={})
+    user = users(user) if user.is_a? Symbol
+    session.merge(:user_id => user.id)
+  end
+  
+  def deactivate_case(message='test-case deactivated')
+    flunk message
+  end
+end
