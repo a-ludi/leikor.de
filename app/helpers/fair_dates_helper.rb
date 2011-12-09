688513d1c9b1446ca1dd846d79a19bc7e21605a3
Arne Ludwig <ludwig.arne@googlemail.com>
2011-12-09 16:32:22 +0100

    exported row template

--- app/helpers/fair_dates_helper.rb
+++ app/helpers/fair_dates_helper.rb
@@ -1,2 +1,9 @@
 module FairDatesHelper
+  def unless_su(string)
+    superuser_logged_in? ? '' : string
+  end
+  
+  def num_rows
+    superuser_logged_in? ? @fair_dates.count + 2 : @fair_dates.count + 1
+  end
 end
