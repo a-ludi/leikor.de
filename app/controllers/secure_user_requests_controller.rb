c0127c6b74f8ce308faaa0732c5b07d4bd4d663e
Arne Ludwig <ludwig.arne@googlemail.com>
2012-03-12 11:14:22 +0100

    Implemented basic behaviour for :confirm_registration

--- app/controllers/secure_user_requests_controller.rb
+++ app/controllers/secure_user_requests_controller.rb
@@ -1,7 +1,33 @@
 class SecureUserRequestsController < ApplicationController
   def edit
+    get_and_set_secure_user_request
+    
+    case @secure_user_request.action
+      when :confirm_registration
+        render 'confirm_registration/edit'
+    end
   end
 
   def update
+    get_and_set_secure_user_request
+    
+    case @secure_user_request.action
+      when :confirm_registration
+        update_confirm_registration
+      else
+        not_found
+    end
+  end
+
+private
+  
+  def update_confirm_registration
+    redirect_to profile_path(@secure_user_request.user.login)
+  end
+  
+  def get_and_set_secure_user_request
+    external_id = params[:id]
+    @secure_user_request = SecureUserRequest.find_by_external_id(external_id) or
+        not_found("No SecureUserRequest with external ID <#{external_id.inspect}> found", true)
   end
 end
