e01092ca07fae7073065b0b996ecbb4d5b50f509
Arne Ludwig <ludwig.arne@googlemail.com>
2012-03-16 10:16:53 +0100

    Class FlashMessage done

--- lib/flash_message.rb
+++ lib/flash_message.rb
@@ -1,28 +1,67 @@
 # encoding: utf-8
 
 class FlashMessage < ActionController::Base
-  @class = nil
-  @title = ''
-  @text = ''
+  include ActionController::Translation
+  DEFAULTS = {:klass => nil, :title => nil, :text => ''}
   
+  attr_reader :text
+  attr_writer :title
   
-  def <<(obj)
-    case obj
-      when String then @text += text
-      when Hash then @text += render_to_string(obj)
+  def klass; @klass.to_s; end
+  
+  def title
+    return @title  unless @title.blank?
+    
+    case @klass
+      when :error then t 'flash_message.title.error'
+      when :success then t 'flash_message.title.success'
+      else t 'flash_message.title.default'
     end
   end
   
-  def self.error(text=nil, title='')
-    FlashMessage << text.to_s
-    @class = :error
+protected
+
+  attr_writer :klass
+  
+public
+  
+  def initialize(options={})
+    options = FlashMessage::DEFAULTS.merge options
+    @klass = options[:klass]
+    @title = options[:title]
+    @text = options[:text]
   end
   
-  def self.success(text=nil, title='')
-    FlashMessage << text.to_s
-    flash[:message]
-    
-    @class = :success
+  def error(text=nil, title=nil)
+    set_message :klass => :error, :title => title, :text => text
+  end
+  
+  def success(text=nil, title=nil)
+    set_message :klass => :success, :title => title, :text => text
+  end
+  
+  def <<(obj)
+    case obj
+      when Hash then @text += render_to_string(obj)
+      else @text += "<p>#{obj.to_s}</p>"
+    end
+  end
+  
+  def empty?
+    @text.blank?
+  end
+  
+  def clear!
+    @text = ''
+    @klass = @title = nil
+  end
+
+private
+
+  def set_message(options)
+    self.klass = options[:klass]
+    self.title = options[:title]
+    self << options[:text]
   end
 end
 
