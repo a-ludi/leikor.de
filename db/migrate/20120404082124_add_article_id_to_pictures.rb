b0703a91b3971d4a72f0fc9febbe94235a9ff377
Arne Ludwig <ludwig.arne@googlemail.com>
2012-04-10 17:35:51 +0200

    Error due to merge

--- db/migrate/20120404082124_add_article_id_to_pictures.rb
+++
@@ -1,9 +0,0 @@
-class AddArticleIdToPictures < ActiveRecord::Migration
-  def self.up
-    add_column(:pictures, :article_id, :integer)
-  end
-
-  def self.down
-    remove_column(:pictures, :article_id)
-  end
-end
--- db/migrate/20120404083945_rename_filename_in_pictures.rb
+++
@@ -1,7 +0,0 @@
-class RenameFilenameInPictures < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
