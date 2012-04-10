46a72b4ffe5b339d4d56fca620734674ac517490
Arne Ludwig <ludwig.arne@googlemail.com>
2012-04-10 17:20:57 +0200

    Introduced blog; made actions new and edit working

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,56 @@
+# -*- encoding : utf-8 -*-
+
+class BlogController < ApplicationController
+  before_filter :employee_required, :except => [:index]
+  
+  def index
+    @title = "Blog"
+    @stylesheets = %w(blog)
+    @blog_posts = BlogPost.all
+  end
+
+  def new
+    @title = "Neuer Blogbeitrag"
+    @stylesheets = %w(message form blog)
+    @blog_post = flash[:blog_post] || BlogPost.new
+    
+    render :edit
+  end
+
+  def create
+    @blog_post = @current_user.owned_blog_posts.create params[:blog_post]
+    @blog_post.editor = @blog_post.author
+    
+    if @blog_post.save
+      redirect_to blog_post_path(@blog_post.public_id)
+    else
+      flash[:blog_post] = @blog_post
+      
+      redirect_to new_blog_post_path
+    end
+  end
+
+  def edit
+    @title = "Blogbeitrag bearbeiten"
+    @stylesheets = %w(message form blog)
+    @blog_post = flash[:blog_post] || BlogPost.find(params[:id])
+  end
+
+  def update
+    @blog_post = BlogPost.find params[:id]
+    @blog_post.update_attributes params[:blog_post]
+    @blog_post.editor = @current_user
+    
+    if @blog_post.save
+      redirect_to blog_post_path(@blog_post.public_id)
+    else
+      flash[:blog_post] = @blog_post
+      
+      redirect_to edit_blog_post_path(@blog_post.public_id)
+    end
+  end
+
+  def destroy
+  end
+
+end
---
+++ app/models/blog_post.rb
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+
+class BlogPost < ActiveRecord::Base
+  set_primary_key :public_id
+  
+  marked_up_with_maruku :body
+  belongs_to :author, :class_name => 'User'
+  belongs_to :editor, :class_name => 'User'
+  
+  validates_presence_of :public_id, :title, :body, :author, :editor
+  validates_format_of :public_id, :with => /[a-z0-9-]+/
+  validates_markdown :body
+  
+  before_validation_on_create :set_public_id
+
+private
+  
+  def set_public_id
+    self[:public_id] ||= title.url_safe
+  end
+end
