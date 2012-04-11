ae3719217347bcc030f3067ffdc46f7b163cd9fe
Arne Ludwig <ludwig.arne@googlemail.com>
2012-04-11 09:43:21 +0200

    Documented FlashMessage

--- lib/flash_message.rb
+++ lib/flash_message.rb
@@ -6,18 +6,35 @@ class FlashMessage < ActionController::Base
   
   attr_writer :title
   
+  # Returns <tt>@klass.to_s</tt>.
   def klass; @klass.to_s; end
   
+  # Returns the title of this message. If not explicitly set <tt>FlashMessage</tt> will try to guess
+  # an apropriate title like <tt>translate @klass.to_s, :scope => 'flash_message.title'<tt>. If none
+  # is found <tt>flash_message.title.default</tt> will be used.
   def title
     return @title  unless @title.blank?
     
-    case @klass
-      when :error then t 'flash_message.title.error'
-      when :success then t 'flash_message.title.success'
-      else t 'flash_message.title.default'
+    custom_or_default_title
+  end
+
+private
+  
+  def custom_or_default_title
+    begin
+      t! @klass.to_s, :scope => 'flash_message.title'
+    rescue I18n::MissingTranslationData
+      t 'flash_message.title.default'
     end
   end
+
+public
   
+  # Returns a hash which can directly be passed to <tt>render</tt>. Typical usage looks like:
+  #
+  #   <div id="message">
+  #     <%= render flash[:message].render_options %>
+  #   </div>
   def render_options
     case @text
       when Hash then @text
@@ -38,14 +55,19 @@ public
     @text = options[:text]
   end
   
-  def error(text=nil, title=nil)
+  # Just pass a text and optionally a title of the error message. If no title is given it will be
+  # looked up at <tt>flash_message.title.error</tt>.
+  def error(text, title=nil)
     set_message :klass => :error, :title => title, :text => text
   end
   
+  # Just pass a text and optionally a title of the (success) message. If no title is given it will
+  # be looked up at <tt>flash_message.title.success</tt>.
   def success(text=nil, title=nil)
     set_message :klass => :success, :title => title, :text => text
   end
   
+  # Append text to the message or set a options <tt>Hash</tt> to be passed to <tt>render</tt>.
   def <<(obj)
     case obj
       when Hash then @text = obj
@@ -53,10 +75,12 @@ public
     end
   end
   
+  # Returns <tt>@text.blank?</tt>.
   def empty?
     @text.blank?
   end
   
+  # Clears text, title and class.
   def clear!
     @text = ''
     @klass = @title = nil
