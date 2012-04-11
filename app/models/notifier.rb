3ad89f1cc830e9476883beeb5536df154f6c6db3
Arne Ludwig <ludwig.arne@googlemail.com>
2012-04-11 22:51:42 +0200

    Mailing blog posts possible; minor refactoring

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -7,12 +7,13 @@ class Notifier < ActionMailer::Base
   layout 'layouts/mail.text'
   
   DEFAULT_SENDER = "notifier@leikor.de"
-  DEFAULT_REPLY_TO = "webmaster@leikor.de"
+  TECHNICAL_SUPPORT = "webmaster@leikor.de"
+  CUSTOMER_SUPPORT = "info@leikor.de"
   
   def reset_password_request(recipient)
     recipients recipient.email_address_with_name
     from       Notifier::DEFAULT_SENDER
-    reply_to   Notifier::DEFAULT_REPLY_TO
+    reply_to   Notifier::TECHNICAL_SUPPORT
     subject    t('activerecord.models.secure_user_request/reset_password')
     body       :user => recipient,
                :request => recipient.reset_password_request
@@ -21,9 +22,18 @@ class Notifier < ActionMailer::Base
   def confirm_registration_request(recipient)
     recipients recipient.email_address_with_name
     from       Notifier::DEFAULT_SENDER
-    reply_to   Notifier::DEFAULT_REPLY_TO
+    reply_to   Notifier::TECHNICAL_SUPPORT
     subject    t('activerecord.models.secure_user_request/confirm_registration')
     body       :user => recipient,
                :request => recipient.confirm_registration_request
+  end
+  
+  def blog_post(recipient, blog_post)
+    recipients recipient.email_address_with_name
+    from       Notifier::DEFAULT_SENDER
+    reply_to   Notifier::CUSTOMER_SUPPORT
+    subject    t('views.notifier.blog_post.title', :title => blog_post.title)
+    body       :user => recipient,
+               :blog_post => blog_post
   end
 end
