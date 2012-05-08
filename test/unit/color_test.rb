f809dafc199cf0f7d953abd12f083e138358d936
Arne Ludwig <ludwig.arne@googlemail.com>
2012-05-08 17:44:36 +0200

    Added tests ColorTest and FairDateTest; removed NotifierTest; debugged FairDate

--- test/unit/color_test.rb
+++ test/unit/color_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class ColorTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should have a unique label" do
+    colors(:green).label = 'red'
+    assert_errors_on colors(:green), :on => :label
+    
+    colors(:red).label = nil
+    assert_errors_on colors(:red), :on => :label
+  end
+
+  test "should have a hex code with correct format" do
+    colors(:green).hex = 'invalid_hex_code'
+    assert_errors_on colors(:green), :on => :hex
+    
+    colors(:red).hex = nil
+    assert_errors_on colors(:red), :on => :hex
   end
 end
