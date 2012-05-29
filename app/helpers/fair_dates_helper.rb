3668f34ecdd2a142f72acc15288a94d2dfa5627c
Arne Ludwig <ludwig.arne@googlemail.com>
2012-05-29 16:49:08 +0200

    Removed unused method

--- app/helpers/fair_dates_helper.rb
+++ app/helpers/fair_dates_helper.rb
@@ -5,10 +5,6 @@ module FairDatesHelper
     logged_in?(Employee) ? '' : string
   end
   
-  def num_rows
-    (logged_in?(Employee) ? @fair_dates.count + 2 : @fair_dates.count + 1) + (@fair_dates.count == 0 ? 1 : 0)
-  end
-  
   def nice_url(raw_url)
     raw_url[ /^([a-zA-Z]+:\/\/)?([^\/]+)/, 2]
   end
