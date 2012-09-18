9bd0e97ccada031d87d770b37a40f817474981e6
Arne Ludwig <ludwig.arne@googlemail.com>
2012-09-18 17:15:11 +0200

    Added confidential information

---
+++ config/initializers/smtp_authentication.rb
@@ -0,0 +1,11 @@
+# -*- encoding : utf-8 -*-
+
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.googlemail.com',
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'ludis-laptop',
+    :user_name => 'arne@leikor.de',
+    :password => 'J17/i$8?',
+    :enable_starttls_auto => true }
+
