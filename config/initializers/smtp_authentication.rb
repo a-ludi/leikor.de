f614d0375b6a25fb689caab683f7deadfe36d49e
Arne Ludwig <ludwig.arne@googlemail.com>
2012-09-18 17:34:45 +0200

    Changed smtp credentials

--- config/initializers/smtp_authentication.rb
+++ config/initializers/smtp_authentication.rb
@@ -5,7 +5,7 @@ ActionMailer::Base.smtp_settings = {
     :port => 587,
     :authentication => :plain,
     :domain => 'ludis-laptop',
-    :user_name => 'arne@leikor.de',
-    :password => 'J17/i$8?',
+    :user_name => 'benachrichtigung@leikor.de',
+    :password => 'DnUz4ANu3jO6oydBeV73ev/E9uY39p7dGqRuN3inwiI',
     :enable_starttls_auto => true }
 
