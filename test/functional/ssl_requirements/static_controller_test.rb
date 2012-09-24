f0214169cadfef2f06d32f1e4b0b792dd8677153
Arne Ludwig <ludwig.arne@googlemail.com>
2012-09-24 14:37:43 +0200

    Checksum updated

--- test/functional/ssl_requirements/static_controller_test.rb
+++ test/functional/ssl_requirements/static_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 module SslRequirements
   class StaticControllerTest < ActionController::TestCase
     test_tested_files_checksum(
-      ['app/controllers/static_controller.rb', 'a6d3812e87c29b9f4c19d787b526d744'],
+      ['app/controllers/static_controller.rb', '31b036aead20f9021c004cc15c0b10f5'],
       ['config/routes.rb', 'a593e4312e018d6a068b0fb01d7ba989']
     )
     
