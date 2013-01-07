f7c5ac26fccafb748dc50eff076d1575ca00ea3a
Arne Ludwig <ludwig.arne@googlemail.com>
2013-01-07 19:34:27 +0100

    Moved confidential data to environment variables

---
+++ config/initializers/smtp_authentication.rb
@@ -0,0 +1,10 @@
+# -*- encoding : utf-8 -*-
+
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.googlemail.com',
+    :port => 587,
+    :authentication => :plain,
+    :domain => ENV['GMAIL_SMTP_USER'],
+    :user_name => ENV['GMAIL_SMTP_USER'],
+    :password => ENV['GMAIL_SMTP_PASSWORD'],
+    :enable_starttls_auto => true }
