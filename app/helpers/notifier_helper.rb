a6b3f1e19fc6db2803627abd001674c0d5e0e73a
Arne Ludwig <ludwig.arne@googlemail.com>
2013-01-07 11:29:04 +0100

    Removed blog_post_with_login_if_required helper - no longer needed

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -7,12 +7,4 @@ module NotifierHelper
     
     host + path[1..-1]
   end
-  
-  def blog_post_url_with_login_if_required blog_post
-    if blog_post.is_published?
-      blog_post_url blog_post
-    else
-      new_session_url(:referer => blog_post_path(blog_post), :protocol => 'https')
-    end
-  end
 end
