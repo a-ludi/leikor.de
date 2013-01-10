f32f5cad1dc99f640f136f415da3ca0e187e9d44
Arne Ludwig <ludwig.arne@googlemail.com>
2013-01-10 17:32:18 +0100

    Moved SMTP address and port to ENV

--- config/initializers/smtp_authentication.rb
+++ config/initializers/smtp_authentication.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.googlemail.com',
-    :port => 587,
+    :address => ENV['GMAIL_SMTP_ADDRESS'],
+    :port => ENV['GMAIL_SMTP_PORT'],
     :authentication => :plain,
     :domain => ENV['GMAIL_SMTP_DOMAIN'],
     :user_name => ENV['GMAIL_SMTP_USER'],
