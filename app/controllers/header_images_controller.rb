7cf7f95bdb3df8a3e89552fcb2181c52af48caa4
Arne Ludwig <ludwig.arne@googlemail.com>
2013-03-27 16:39:01 +0100

    Mininmal refactoring

--- app/controllers/header_images_controller.rb
+++ app/controllers/header_images_controller.rb
@@ -1,13 +1,14 @@
 class HeaderImagesController < ApplicationController
   ssl_required_by_all_actions
   HASH_KEY_MAP = {'links' => 'left_header_image_path', 'rechts' => 'right_header_image_path'}
+  HEADER_IMAGE_STYLE = :thumb
   before_filter :validate_id
   
   def update
     @article = Article.find_by_article_number params[:article_number]
     @hash_key = self.class::HASH_KEY_MAP[params[:id]]
     
-    AppData[@hash_key] = @article.nil? ? '' : @article.picture.url(:thumb)
+    AppData[@hash_key] = @article.nil? ? '' : @article.picture.url(HEADER_IMAGE_STYLE)
     
     flash[:message].success "Seitenkopfbild (#{params[:id]}) aktualisiert."
     
