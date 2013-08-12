cd191084b206fb3a8cf411614ab4d33e1ad76fc6
Arne Ludwig <ludwig.arne@googlemail.com>
2013-08-12 17:10:43 +0200

    Bugfix in DBStorage

--- lib/plugins/paperclip/lib/storage.rb
+++ lib/plugins/paperclip/lib/storage.rb
@@ -3,76 +3,76 @@ module Paperclip
   module Storage
 
     # Store files in a database.
-    # 
+    #
     # Usage is identical to the file system storage version, except:
-    # 
+    #
     # 1. In your model specify the "database" storage option; for example:
     #   has_attached_file :avatar, :storage => :database
-    # 
+    #
     # 2. The file will be stored in a column called [attachment name]_file (e.g. "avatar_file") by default.
-    # 
+    #
     # To specify a different column name, use :column, like this:
     #   has_attached_file :avatar, :storage => :database, :column => 'avatar_data'
-    # 
+    #
     # If you have defined different styles, these files will be stored in additional columns called
     # [attachment name]_[style name]_file (e.g. "avatar_thumb_file") by default.
-    # 
+    #
     # To specify different column names for styles, use :column in the style definition, like this:
     #   has_attached_file :avatar,
     #                     :storage => :database,
-    #                     :styles => { 
+    #                     :styles => {
     #                       :medium => {:geometry => "300x300>", :column => 'medium_file'},
     #                       :thumb => {:geometry => "100x100>", :column => 'thumb_file'}
     #                     }
-    # 
+    #
     # 3. You need to create these new columns in your migrations or you'll get an exception. Example:
     #   add_column :users, :avatar_file, :binary
     #   add_column :users, :avatar_medium_file, :binary
     #   add_column :users, :avatar_thumb_file, :binary
-    # 
+    #
     # Note the "binary" migration will not work for the LONGBLOB type in MySQL for the
     # file_contents column. You may need to craft a SQL statement for your migration,
     # depending on which database server you are using. Here's an example migration for MySQL:
     #   execute 'ALTER TABLE users ADD COLUMN avatar_file LONGBLOB'
     #   execute 'ALTER TABLE users ADD COLUMN avatar_medium_file LONGBLOB'
     #   execute 'ALTER TABLE users ADD COLUMN avatar_thumb_file LONGBLOB'
-    # 
+    #
     # 4. To avoid performance problems loading all of the BLOB columns every time you access
     # your ActiveRecord object, a class method is provided on your model called
     # “select_without_file_columns_for.” This is set to a :select scope hash that will
     # instruct ActiveRecord::Base.find to load all of the columns except the BLOB/file data columns.
-    # 
+    #
     # If you’re using Rails 2.3, you can specify this as a default scope:
     #   default_scope select_without_file_columns_for(:avatar)
-    # 
+    #
     # Or if you’re using Rails 2.1 or 2.2 you can use it to create a named scope:
     #   named_scope :without_file_data, select_without_file_columns_for(:avatar)
-    # 
+    #
     # 5. By default, URLs will be set to this pattern:
     #   /:relative_root/:class/:attachment/:id?style=:style
-    # 
+    #
     # Example:
     #   /app-root-url/users/avatars/23?style=original
-    # 
+    #
     # The idea here is that to retrieve a file from the database storage, you will need some
     # controller's code to be executed.
-    #     
+    #
     # Once you pick a controller to use for downloading, you can add this line
     # to generate the download action for the default URL/action (the plural attachment name),
     # "avatars" in this example:
     #   downloads_files_for :user, :avatar
-    # 
+    #
     # Or you can write a download method manually if there are security, logging or other
     # requirements.
-    # 
+    #
     # If you prefer a different URL for downloading files you can specify that in the model; e.g.:
     #   has_attached_file :avatar, :storage => :database, :url => '/users/show_avatar/:id/:style'
-    # 
+    #
     # 6. Add a route for the download to the controller which will handle downloads, if necessary.
-    # 
+    #
     # The default URL, /:relative_root/:class/:attachment/:id?style=:style, will be matched by
     # the default route: :controller/:action/:id
-    # 
+    #
     module Database
       def self.extended base
         base.instance_eval do
@@ -95,7 +95,7 @@ module Paperclip
         end
         ActiveRecord::Base.logger.info("[paperclip] Database Storage Initalized.")
       end
-      
+
       def make_file_columns_from_styles
         file_columns = {}
         options.styles.each_key do |style|
@@ -109,7 +109,7 @@ module Paperclip
       def column_for_style style
         @file_columns[style.to_sym]
       end
-        
+
       def instance_read_file(style)
         ActiveRecord::Base.logger.debug "[paperclip] reading file with style <#{style.inspect}>"
         ActiveRecord::Base.logger.debug "[paperclip] file_columns <#{@file_columns.inspect}>"
@@ -147,6 +147,7 @@ module Paperclip
         elsif exists?(style)
           tempfile = Tempfile.new instance_read(:file_name)
           tempfile.write file_contents(style)
+          tempfile.rewind
           tempfile
         else
           nil
@@ -194,7 +195,7 @@ module Paperclip
       def flush_deletes
         @queued_for_delete = []
       end
-      
+
       module ControllerClassMethods
         def self.included(base)
           base.extend(self)
